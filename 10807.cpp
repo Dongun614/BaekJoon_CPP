8a4b1c7e023a45e29dc37c709a376fd2a6c5f0a2
Dongun614 <72856107+Dongun614@users.noreply.github.com>
2025-07-23 23:33:18 +0900

    Feat: create 10807.cpp

---
+++ 10807.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+//입력할 숫자의 개수를 받음
+//갯수만큼 int 배열을 malloc을 통해서 만든다.
+//for문을 통해 숫자를 받는다
+//찾아야하는 숫자를 입력받는다.
+//count라는 값 만든다
+//for문 돌면서 찾아야할 값이랑 같으면 count 값을 증가시킨다
+//count 값을 출력한다.
+
+
+int main(){
+
+    return 0;
+}
