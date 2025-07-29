ca09bb084e565824bf9f1bea6a45aa0668b7fa48
Dongun614 <72856107+Dongun614@users.noreply.github.com>
2025-07-29 22:36:15 +0900

    Feat: complete 15963

--- 15963.cpp
+++ 15963.cpp
@@ -3,6 +3,14 @@
 using namespace std;
 
 int main(){
+    long long N, M;
+    cin >> N >> M;
+
+    if(N == M){
+        cout << 1 << endl;
+    } else {
+        cout << 0 << endl;
+    }
 
     return 0;
 }
