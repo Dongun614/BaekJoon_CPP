ac05198d6b21bd92e55f8057ffe07ee3be776e57
Dongun614 <jdw215415@gmail.com>
2025-08-03 18:12:31 +0900

    feat: finish 28961.cpp

--- 28961.cpp
+++ 28961.cpp
@@ -4,21 +4,19 @@
 using namespace std;
 
 int main(){
-    string club[5] = {"MatKor", "WiCys", "CyKor", "AlKor", "$clear"};
-
-    string input;
+    char input;
     cin >> input;
 
-    if(input == "M"){
-        cout << club[0] << endl;
-    } else if(input == "W"){
-        cout << club[1] << endl;
-    } else if(input == "C"){
-        cout << club[2] << endl;
-    } else if(input == "A"){
-        cout << club[3] << endl;
-    } else if(input == "$"){
-        cout << club[4] << endl;
+    if(input == 'M'){
+        cout << "MatKor" << endl;
+    } else if(input == 'W'){
+        cout << "WiCys" << endl;
+    } else if(input == 'C'){
+        cout << "CyKor" << endl;
+    } else if(input == 'A'){
+        cout << "AlKor" << endl;
+    } else if(input == '$'){
+        cout << "$clear" << endl;
     } else {
         cout << "잘못된 입력입니다" << endl;
     }
