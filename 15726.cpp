7e5e1775b3db401de883b864a6692076191449e9
Dongun614 <jdw215415@gmail.com>
2025-08-05 19:41:07 +0900

    Feat: 15726.cpp 완료

--- 15726.cpp
+++ 15726.cpp
@@ -2,10 +2,6 @@
 
 using namespace std;
 
-//1. 세 수를 입력받는다.
-//2. 나누고 곱하고, 곱하고 나누고 두 개를 계산한다.
-//3. 더 큰 값을 출력한다.
-
 int main(){
     int A, B, C;
     cin >> A >> B >> C;
@@ -13,13 +9,13 @@ int main(){
     int division_fist;
     int multiple_first;
 
-    division_fist = A / B * C;
-    multiple_first = A * B / C;
+    division_fist = ((double) A / B) * C;
+    multiple_first = ((double) A * B) / C;
 
     if(division_fist < multiple_first){
-        printf("%d\n", multiple_first);
+        cout << multiple_first << endl;
     } else {
-        printf("%d\n", division_fist;
+        cout << division_fist << endl;
     }
 
     return 0;
