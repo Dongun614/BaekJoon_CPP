65bc7623e3a38a2c436ddd193ac900ce7c19c878
Dongun614 <jdw215415@gmail.com>
2025-08-06 16:43:29 +0900

    Feat: 3003.cpp

---
+++ 3003.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+#define PIECE_NUMBER 6
+
+int main(){
+    int limit[PIECE_NUMBER] = {1, 1, 2, 2, 2, 8};
+    int temp;
+
+    for(int i=0; i<PIECE_NUMBER; i++){
+        cin >> temp;
+        cout << limit[i] - temp << endl;
+    }    
+
+    return 0;
+}
