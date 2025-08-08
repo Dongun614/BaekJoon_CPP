7a116833df01bbe2789eeac16154d4f24528b74d
Dongun614 <jdw215415@gmail.com>
2025-08-08 17:41:11 +0900

    Feat: 14645.cpp 완성

---
+++ 14645.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int inter, start_mem;
+    int plus_mem, minus_mem;
+
+    cin >> inter >> start_mem;
+
+    for(int i=0; i<inter; i++){
+        cin >> plus_mem >> minus_mem;
+    }
+
+    cout << "비와이" << endl;
+
+    return 0;
+}
