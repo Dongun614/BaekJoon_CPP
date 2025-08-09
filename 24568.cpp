3279ebe1db3cec01c34d59623e04278cf15b27f6
Dongun614 <jdw215415@gmail.com>
2025-08-09 15:42:57 +0900

    feat: 24568.cpp

---
+++ 24568.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+#define STUDENT_NUM 28
+#define REGULAR 8
+#define SMALL 8
+
+int cal_left(int regular, int small);
+
+int main(){
+    int reg, sml;
+    cin >> reg >> sml;
+
+    int left_cake;
+    left_cake = cal_left(reg, sml);
+
+    cout << left_cake << endl;
+    return 0;
+}
+
+int cal_left(int regular, int small){
+    int ret;
+    regular = regular * REGULAR;
+    small = small * SMALL;
+    ret = (regular + small) / STUDENT_NUM;
+
+    return ret;
+}
