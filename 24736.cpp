2cbc5f85e4303c219b9214231fd3b98805616d67
Dongun614 <jdw215415@gmail.com>
2025-08-11 16:15:50 +0900

    feat: complete 24736.cpp

--- 24736.cpp
+++ 24736.cpp
@@ -1,25 +1,33 @@
 #include <iostream>
 
 using namespace std;
-//스코어 지표를 int 배열로 선언
-//team_a, team_b라는 종합 점수 저장할 변수 선언
-//반복문 통해서 5번 temp 값을 받아서 int 배열 인덱스랑 i값 이용해서 종합 점수 누산하기
-//이후 두 값 출력
+
+#define TEAM_NUM 2
+#define TEAM_A 0
+#define TEAM_B 1
+
+#define SCORE_TYPE 5
+#define TOUCH_DOWN 6
+#define FIELD_GOAL 3
+#define SAFETY 2
+#define POINT_AFTER 1
+#define TWO_POINT_CONVERSION 2
+
 
 int main(){
-    int team_score[2] = {0,};
-    int score[6] = {6, 3, 2, 1, 2};
+    int team_score[TEAM_NUM] = {0,};
+    int score[SCORE_TYPE] = {TOUCH_DOWN, FIELD_GOAL, SAFETY, POINT_AFTER, TWO_POINT_CONVERSION};
     int temp;
 
-    for(int i=0; i<2; i++){
-        for(int j=0; j<6; j++){
+    for(int i=0; i<TEAM_NUM; i++){
+        for(int j=0; j<SCORE_TYPE; j++){
             cin >> temp;
             temp = temp * score[j];
             team_score[i] += temp;
         }
     }
 
-    cout << team_score[0] << " " << team_score[1] << endl;
+    cout << team_score[TEAM_A] << " " << team_score[TEAM_B] << endl;
 
     return 0;
 }
