4844386db6c558da1f5d139afb32edbb492273c0
Dongun614 <jdw215415@gmail.com>
2025-08-12 17:41:55 +0900

    Feat: 13752 완료

---
+++ 13752.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int num; //입력받을 숫자의 총 개수
+    int histo; //히스토그램 길이 입력받을 변수
+
+    cin >> num;
+
+    for(int i=0; i<num; i++){
+        cin >> histo;
+        for(int j=0; j<histo; j++){
+            cout << "=" ;
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
+
