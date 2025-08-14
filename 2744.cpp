16e954a60366c332c1c9d5b22ad16e823dfcc43d
Dongun614 <jdw215415@gmail.com>
2025-08-14 16:08:23 +0900

    Feat: complete 2744.cpp

---
+++ 2744.cpp
@@ -0,0 +1,26 @@
+//참고
+//https://notepad96.tistory.com/64
+
+#include <iostream>
+#include <string>
+#include <cctype>
+
+using namespace std;
+
+int main(){
+    string s;
+    cin >> s;
+
+    for(int i=0; i<s.size(); i++){
+        if(islower(s[i])){
+            s[i] = toupper(s[i]);
+        } else if(isupper(s[i])){
+            s[i] = tolower(s[i]);
+        }
+    }
+
+    cout << s << endl;
+
+
+    return 0;
+}
