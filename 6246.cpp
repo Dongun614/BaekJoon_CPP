f221a3a9e11c7c7a78c88631b56d953392a8204b
Dongun614 <jdw215415@gmail.com>
2025-08-20 11:15:56 +0900

    Feat: make 6246.cpp

---
+++ 6246.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int slot, turn;
+    int start, space;
+
+    cin >> slot >> turn;
+
+    int* row = (int*)malloc(sizeof(int)*slot);
+    memset(row, 0, sizeof(int) * slot);
+
+    for(int i=0; i<turn; i++){
+        cin >> 
+    }
+
+    return 0;
+}
