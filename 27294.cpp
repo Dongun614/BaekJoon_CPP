18c12be96171fd32041c1361e2d8b557d18614be
Dongun614 <jdw215415@gmail.com>
2025-09-04 19:40:48 +0900

    Feat: 27294.cpp make

--- 27294.cpp
+++ 27294.cpp
@@ -1,8 +1,19 @@
+//술하고 같이 초밥을 먹거나 점심식사가 아닐때 -> 280
+//점심식사(12~16)이면서 술과 같이 먹지 않을 때 -> 320
+
 #include <iostream>
 
 using namespace std;
 
 int main(){
+    int T, S;
+    cin >> T >> S;
+
+    if(S == 1 || !(T >= 12 && T <= 16)){
+        cout << 280 << endl;
+    }else if(S == 0 && (T >= 12 && T <= 16)){
+        cout << 320 << endl;
+    }
 
     return 0;
 }
