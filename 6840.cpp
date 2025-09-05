bedbc33e4dc2113f414015ec25b1193d78ea48d3
Dongun614 <jdw215415@gmail.com>
2025-09-05 12:03:04 +0900

    Feat: 6840.cpp 작성 중

--- 6840.cpp
+++ 6840.cpp
@@ -1,10 +1,47 @@
+//Quick sort 사용하기 
+//https://gmlwjd9405.github.io/2018/05/10/algorithm-quick-sort.html
+
 #include <iostream>
 
 using namespace std;
 
-int main(){
+void quicksort(int list[], int left, int right);
+void SWAP(int a, int b);
 
-    
+#define NUMBER 3
+int main(){
+    int num[NUMBER];
+
+    for(int i=0; i<NUMBER; i++){
+        cin >> num[i];
+    }
+
+    //1. 리스트가 들어온다.
+    //2. 첫번째 요소를 pivot으로 잡는다.
+    //3. 두번째 요소와 맨 마지막 요소가 각각 left, right가 된다.
+    //4. 순차적으로 이름과 맞지 않는 요소를 찾는다.
+    //5. 각각 찾으면 둘은 서로 교환된다.
+
 
     return 0;
+}
+
+void quicksort(int list[], int low, int high){
+    int pivot = list[0];
+
+    while(pivot > list[low]) low++;
+    while(pivot < list[high]) high--;
+
+    if(low < high){
+        SWAP(low, high);
+    }else{
+        SWAP(pivot, high);
+    }
+}
+
+void SWAP(int a, int b){
+    int temp;
+    temp = a;
+    a = b;
+    b = temp;
 }
