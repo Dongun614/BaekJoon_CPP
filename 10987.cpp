91490ab897aebc105503259be7ebdf6a74f2cd19
Dongun614 <jdw215415@gmail.com>
2025-09-21 22:24:19 +0900

    feat: ㅈ석

--- 10987.cpp
+++ 10987.cpp
@@ -3,6 +3,17 @@
 using namespace std;
 
 int main(){
+    string str;
+    int count = 0;
+    cin >> str;
+
+    for(int i=0; i<str.length(); i++){
+        if(str[i] == 'a' || str[i] == 'e' || str[i] == 'i' || str[i] == 'o' || str[i] == 'u'){
+            count++;
+        }
+    }
+
+    cout << count << endl;
 
     return 0;
-}
+}//
