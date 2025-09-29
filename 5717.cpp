41e167ef1f2df00d9214c4b15c9ce31277c3054c
Dongun614 <jdw215415@gmail.com>
2025-09-29 22:57:03 +0900

    Feat: complete 5717.cpp

--- 5717.cpp
+++ 5717.cpp
@@ -3,6 +3,15 @@
 using namespace std;
 
 int main(){
+    int female, male;
+    
+    while(1){
+        cin >> female >> male;
+        if(female == 0 && male == 0){
+            break;
+        }
+        cout << female + male << endl;
+    }
 
     return 0;
 }
